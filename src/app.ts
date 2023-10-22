2a6aeaafad24bf5759a69fe6f902f0aa378921d4
Cade Weiskopf <cadew@pm.me>
2023-10-21 17:59:17 -0700

    removing unnecessary stuff that might make it to its own project.

--- src/app.ts
+++ src/app.ts
@@ -2,24 +2,8 @@ import express from "express";
 import http from "http";
 import { CHAT_SERVICE_PORT } from "./env-vars";
 import { IWsClient, newWebsocketServer } from "./websocket";
-import fs from "fs";
-
-process.chdir("./dist");
-
-const joinChatPage = fs.readFileSync("./public/pages/join-chat.html", "utf-8");
-const chatPage = fs.readFileSync("./public/pages/chat.html", "utf-8");
 
 const app = express();
-
-app.use(express.static("./public"));
-
-app.get("/chat", (req, res) => {
-  res.send(joinChatPage);
-});
-
-app.post("/chat", (req, res) => {
-  res.send(chatPage);
-});
 
 app.get("*", (req, res) => {
   res.writeHead(404);
